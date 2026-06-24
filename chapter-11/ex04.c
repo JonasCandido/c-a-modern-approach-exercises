d19cd70635bb7090ba52e25afd96e2f0ebb80c6e
JonasCandido <jonasdcandido210@gmail.com>
2026-06-24 12:39:26 -0300

    Add exercise 04 of chapter 11

---
+++ chapter-11/ex04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int *p, int *q);
+
+int main() {
+    int i, j;
+    i = 0;
+    j = 1;
+    printf("i == %d | j == %d\n", i, j);
+    swap(&i, &j);
+    printf("i == %d | j == %d\n", i, j);
+    return 0;
+}
+
+void swap(int *p, int *q) {
+    int temp = *p;
+    *p = *q;
+    *q = temp; 
+}
